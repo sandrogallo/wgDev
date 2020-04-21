c661096be0ad6ef44dd7e2d9632389901ad15530
Zaid1710 <zaid1234@hotmail.it>
2020-04-21 15:48:39 +0200

    Riunione 21/04/2020 - GUI Server
    Co-Authored-By: Zaid Cheikh <Zaid1710@users.noreply.github.com>
    Co-Authored-By: NotXia <NotXia@users.noreply.github.com>
    Co-Authored-By: Manuel Paris <parismanuel@users.noreply.github.com>

---
+++ Server/server_three/ServerThree.py
@@ -0,0 +1,100 @@
+#
+# Server Two
+# Team: Server Python
+# last-updated: 17/04/2020 by Team Server
+#
+
+import socket
+import threading
+import mylib as ml
+from datetime import datetime
+
+# Vettore che memorizza tutte le connessioni
+connessioni = []
+
+# Definizione del lock
+threadLock = threading.Lock()
+
+#
+# Messaggio di chiusura forzata del client
+#
+def client_closed(my_socket, username):
+    now = datetime.now().strftime("%d-%m-%Y %H:%M:%S") # Data corrente
+    out = "["+now+"] " + username + " ha chiuso il client"
+    print(out)
+    send_to_all(my_socket, username, out)
+    connessioni.remove(my_socket)
+
+#
+# Inoltra a tutti gli altri client il messaggio
+#
+def send_to_all(my_socket, username, msg):
+    for socket in connessioni:
+        try:
+            if(socket != my_socket):
+                ml.strSend(socket, username + "> " + msg)
+        except:
+            print(socket, "non ha ricevuto")
+
+class Service(threading.Thread):
+    def __init__(self, s, a, username): # s: clientSocket - a: address
+       threading.Thread.__init__(self)
+       self.s = s
+       self.a = a
+       self.username = username
+
+    def run(self):
+        try:
+            while True:
+                msg = ml.strReceive(self.s) # Ricezione messaggio
+
+                now = datetime.now().strftime("%d-%m-%Y %H:%M:%S") # Data corrente
+
+                # Comando di uscita, il thread viene terminato
+                if msg == 'quit':
+                    out = "["+now+"] " + self.username + " ha abbandonato la chat"
+                    print(out)
+                    ml.strSend(self.s, out) # Messaggio di termine
+                    send_to_all(self.s, self.username, out)
+                    self.s.close()
+                    threadLock.acquire() # Blocca l'accesso alle variabili per gli altri thread
+                    connessioni.remove(self.s)
+                    threadLock.release() # Rilascia il blocco
+                    break;
+
+                print("["+now+"]", self.username + ": " + msg)
+                send_to_all(self.s, self.username, msg)
+        except ConnectionResetError: # Nel caso il client venga chiuso dalla X
+            client_closed(self.s, self.username)
+        except ValueError: # Nel caso il client venga chiuso dalla X
+            client_closed(self.s, self.username)
+
+
+#
+# Creazione socket e ascolto su HOST e PORT
+#
+serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serverSocket.bind((ml.HOST, ml.PORT))
+print("Server Two avviato su", ml.HOST, ":", ml.PORT)
+
+# Il server ascolta
+serverSocket.listen(5)
+while True:
+    try:
+        # Attesa di una connessione
+        print("\nAttesa di una connessione...\n")
+        (clientSocket, addr) = serverSocket.accept()
+
+        username = ml.strReceive(clientSocket)
+        # Connessione ricevuta, estrae l'username
+        print("\nConnessione di", username, "con parametri", addr)
+        connessioni.append(clientSocket)
+
+        # Creazione thred, avvio e ritorno ad ascoltare
+        svc = Service(clientSocket, addr, username)
+        svc.start()
+        print("Thread inizializzato con successo")
+    except ConnectionResetError:
+        print("Client chiuso forzatamente dall'utente")
+
+print("Server terminated.")
---
+++ Server/server_three/test_noip/ClientTwo.py
@@ -0,0 +1,69 @@
+#
+# ClientTwo for Server Two
+# Team: Server Python
+# last-updated: 02/04/2020 by Team Server
+#
+
+import socket
+import threading
+import mylib as ml
+
+HOST = "pinco1710.ddns.net"
+
+#
+# Thread per ascoltare i messaggi in entrata
+#
+class Listener(threading.Thread):
+    def __init__(self, s): # s: clientSocket - a: address
+       threading.Thread.__init__(self)
+       self.s = s
+       self.running = True;
+
+    def run(self):
+        while self.running:
+            msg = ml.strReceive(self.s)
+            print(msg)
+
+    def stop(self):
+        self.running = False
+
+
+# Creazione socket
+clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print("Client connesso a", HOST)
+
+#
+# Connessione a HOST e PORT
+#
+clientSocket.connect((HOST, ml.PORT))
+print("Connesso al server", HOST, ":", ml.PORT)
+
+#
+# Inserimento username
+#
+username = input("Username: ")
+ml.strSend(clientSocket, username) # Il primo messaggio che il server si aspetta è l'username
+
+#
+# Avvio thread che ascolta i messaggi in entrata
+#
+listener = Listener(clientSocket)
+listener.start()
+
+while True:
+    # Input del messaggio dell'utente
+    msg = input("")
+
+    if(len(msg) != 0): # Verifica se il messaggio è vuoto
+        # Invio del messaggio
+        ml.strSend(clientSocket, msg)
+
+        # Arresto del client
+        if msg=="quit":
+            listener.stop()
+            listener.join()
+            break
+
+# Chiude il socket
+clientSocket.close()
+print("Connessione terminata.")
